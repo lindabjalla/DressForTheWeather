5bf78d73c6b6fb28931aa66bd15008c2b367cabc
lindabjalla <lindabjalla@gmail.com>
2016-04-25 11:27:25 +0200

    börjar att skapa WeatherData-klass

--- app/src/main/java/se/hayumi/dressfortheweather/MainActivity.java
+++ app/src/main/java/se/hayumi/dressfortheweather/MainActivity.java
@@ -2,8 +2,13 @@ package se.hayumi.dressfortheweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
