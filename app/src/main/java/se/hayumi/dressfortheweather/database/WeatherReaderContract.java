bdf93c0143df367ffb9b920509d49e83e212a6bf
HArbietoCortez <arbietohaydee@hotmail.com>
2016-04-27 13:39:41 +0200

    Merge branch 'master' of https://github.com/lindabjalla/DressForTheWeather
    Conflicts:
    	app/src/main/res/layout/activity_main.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/se/hayumi/dressfortheweather/database/WeatherReaderContract.java
+++ app/src/main/java/se/hayumi/dressfortheweather/database/WeatherReaderContract.java
@@ -1,22 +1,24 @@
 package se.hayumi.dressfortheweather.database;
 
-import android.content.Context;
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
-public final class WeatherReaderContract {
+import se.hayumi.dressfortheweather.storage.FeedReaderDbHelper;
 
+public final class WeatherReaderContract {
+    
     public WeatherReaderContract() {}
 
     public static abstract class WeatherEntry implements BaseColumns {
         public static final String TABLE_NAME = "WeatherData";
-        public static final String WEATHER_ID = "weatherId";
-        public static final String DATE_TIME = "dateTime";
-        public static final String CONDITION = "condition";
-        public static final int TEMPERATURE = 20;
-        public static final int FEELS_LIKE_TEMPERATURE = 25;
-        public static final boolean NEEDS_UMBRELLA = true;
-        public static final String SUITABLE_CLOTHES = "";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryId";
+        public static final String COLUMN_NAME_DATE_TIME = "dateTime";
+        public static final String COLUMN_NAME_CONDITION = "condition";
+        public static final int COLUMN_NAME_TEMPERATURE = 20;
+        public static final int COLUMN_NAME_FEELS_LIKE_TEMPERATURE = 25;
+        public static final String COLUMN_NAME_CLOTHES_TO_WEAR = "";
+        //public static final String COLUMN_NAME_FETCHED_TIME = " ";
     }
 
     //Metainformation för DataBasen
@@ -24,20 +26,38 @@ public final class WeatherReaderContract {
     private static final String INT_TYPE = "integer";
     private static final String BOOLEAN_TYPE = "boolean";
     private static final String COMMA_SEP = ",";
-    //private static final String SPACE = " ";
+    private static final String SPACE = " ";
 
-    private static final String SQL_CREATE_TABLE_WEATHER =
+    public static final String SQL_CREATE_TABLE_WEATHER =
             "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                    WeatherEntry.WEATHER_ID + "INTEGER PRIMARY KEY," +
-                    WeatherEntry.DATE_TIME + " " + STRING_TYPE + COMMA_SEP +
-                    WeatherEntry.CONDITION + " " + STRING_TYPE + COMMA_SEP +
-                    WeatherEntry.TEMPERATURE + " " + INT_TYPE + COMMA_SEP +
-                    WeatherEntry.FEELS_LIKE_TEMPERATURE + " " + INT_TYPE + COMMA_SEP +
-                    WeatherEntry.NEEDS_UMBRELLA + " " + BOOLEAN_TYPE + COMMA_SEP +
-                    WeatherEntry.SUITABLE_CLOTHES + " " + STRING_TYPE + COMMA_SEP + " )";
+                              WeatherEntry.COLUMN_NAME_ENTRY_ID + "INTEGER PRIMARY KEY," +
+                              WeatherEntry.COLUMN_NAME_DATE_TIME + SPACE + STRING_TYPE + COMMA_SEP +
+                              WeatherEntry.COLUMN_NAME_CONDITION + SPACE + STRING_TYPE + COMMA_SEP +
+                              WeatherEntry.COLUMN_NAME_TEMPERATURE + SPACE + INT_TYPE + COMMA_SEP +
+                              WeatherEntry.COLUMN_NAME_FEELS_LIKE_TEMPERATURE + " " + INT_TYPE + COMMA_SEP +
+                              WeatherEntry.COLUMN_NAME_CLOTHES_TO_WEAR + SPACE + STRING_TYPE + COMMA_SEP + " )";
 
 
-    private static final String SQL_DELETE_TABLE_WEATHER =
+    public static final String SQL_DELETE_TABLE_WEATHER =
             "DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME;
+
+    public void addValues(String dateTime, String condition, int temperature, int feelsLikeTemperature){
+
+        //Create and/or open the database for writing
+        SQLiteDatabase database = WeatherDbHelper.getWritableDatabase();
+
+        database.beginTransaction();
+
+        ContentValues values = new ContentValues();
+        values.put(WeatherEntry.COLUMN_NAME_ENTRY_ID, entryId):
+        values.put(WeatherEntry.COLUMN_NAME_DATE_TIME, dateTime);
+        values.put(WeatherEntry.COLUMN_NAME_CONDITION, condition);
+       // values.put(WeatherEntry.COLUMN_NAME_TEMPERATURE, temperature);
+        //values.put(WeatherEntry.COLUMN_NAME_FEELS_LIKE_TEMPERATURE, feelsLikeTemperature);
+
+        long newRowId;
+       // newRowId = database.insert(WeatherEntry.TABLE_NAME,
+         //                          WeatherEntry.COLUMN_NAME_NULLABLE, values);
+    }
 
 }
