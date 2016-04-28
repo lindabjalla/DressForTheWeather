78399438c5e6978460b9b98327c746e3b2b16073
HArbietoCortez <arbietohaydee@hotmail.com>
2016-04-28 18:32:45 +0200

    GoogleMap ...

---
+++ app/src/main/java/se/hayumi/dressfortheweather/service/MapsActivity.java
@@ -0,0 +1,36 @@
+package se.hayumi.dressfortheweather.service;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add a marker in Sweden and move the camera
+        LatLng stockholm = new LatLng(59.33, 18);
+        map.addMarker(new MarkerOptions().position(stockholm).title("You see Stockholm"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+    }
+}
